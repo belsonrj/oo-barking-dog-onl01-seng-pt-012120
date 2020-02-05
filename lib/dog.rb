138fe15b7b29d05a27244c9b56acaf5575625289
Rick Belson <belsonrj@aol.com>
2020-02-05 02:03:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,14 @@
-# Your code goes here!
-class Dog 
-  def self.name=(dog_name)
-    this_dogs_name = dog_name 
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
   end
   
-  def name
-    this_dogs_name
+  def bark
+    puts "woof!"
   end
 end
